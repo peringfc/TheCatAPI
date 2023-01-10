bffdec43a4a66f336e00a5984e496d5cb8625af4
Perin Christian <peringfc@outlook.com>
2023-01-09 23:33:27 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/BreedController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheCatAPI_Lib.Desafio.search;
+
+namespace TheCatAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BreedController : Controller
+    {
+        /// <summary>
+        /// Lista Racas registrada no banco de dados retorna no formato desajado pelo usuario Json ou XML
+        /// </summary>
+        /// <param name="Format"> Json ou XML</param>
+        /// <returns></returns>
+        [HttpPost(Name = "ListBreeds")]
+        public string ListBreeds(string p_breed, string Format)
+        {
+            string TxtRetorn = string.Empty;
+            try
+            {
+                Cat oCat = new Cat();
+                TxtRetorn = oCat.info_breed(p_breed, Format.ToUpper().Trim().TrimEnd());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return TxtRetorn;
+        }
+    }
+}
---
+++ Controllers/TheCatAPIController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheCatAPI_Lib.Structure.SiteTheCatAPI;
+using TheCatAPI_Lib.Structure.Tools;
+using TheCatAPI_Lib.TheCatAPI.SiteAPI; 
+
+namespace TheCatAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TheCatAPIController : Controller
+    {
+        /// <summary>
+        /// Metodo estara realizando um pesquisa na fonte de dados e retornado um objeto 
+        /// normatizado e serelizado
+        /// </summary>
+        /// <returns> Objeto na estrutura de POC Stru_Breed de nossa biblioteca normatizada</returns>
+        [HttpPost(Name = "Load_Data_TheCatAPI_SIT")]
+        public List<Stru_Log> LoadTheCatAPI()
+        {
+            List<Stru_Log> ListStruLog = new List<Stru_Log>();
+            try
+            {
+                
+                Synchronism oSynchronism = new Synchronism();
+                ListStruLog = oSynchronism.LoadTheCatAPI();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return ListStruLog; 
+        }
+    }
+}
